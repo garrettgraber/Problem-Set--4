aa3983c6424d00f2cb9c74e1bcd1877df8cbc183
garrettgraber <garrett_graber@yahoo.com>
2014-07-22 15:59:18 -0600

    All done

--- problem-set-4.js
+++ problem-set-4.js
@@ -1,74 +1,6 @@
+var _ = require('underscore');
 
-
-var addNumbers = function(inString) {
-
-	var numberArrayFound = [];
-
-	for(var i=2; i < process.argv.length; i++) {
-
-		var stringUse = process.argv[ i ];
-		var previousNumber = '';		
-
-		for(var j=0; j < stringUse.length; j++) {
-		
-			var currentString = stringUse[ j ];
-			var currentArray = currentString.split('');
-			var numberArray = currentArray.filter(function(element, index){
-				return isNaN(element) === false;
-			});
-			
-			// console.log(numberArray);
-
-			if(numberArray.length > 0) {
-				var numberOut = numberArray[ 0 ];
-				numberArrayFound.push([numberOut, j]);
-			}
-		}
-
-	}
-
-	console.log(numberArrayFound);
-	var masterArray = [];
-	var previousPosition = -2;
-	var tempNumber = '';
-
-	for(var i=0; i < numberArrayFound.length; i++) {
-		var currentPosition = numberArrayFound[ i ][ 1 ];
-		var currentValue = numberArrayFound[ i ][ 0 ];
-		console.log('\ncurrentValue: ' + currentValue);
-		console.log('currentPosition: ' + currentPosition);
-		console.log('previousPosition: ' + previousPosition);
-		console.log('tempNumber: ' + tempNumber);
-
-		if(currentPosition - 1 !== previousPosition && previousPosition !== -2) {
-			console.log('singleton');
-			var tempNumber = currentValue;
-			console.log('tempNumber: ' + tempNumber);
-		}
-
-		if(currentPosition - 1 === previousPosition || previousPosition === -2) {
-			console.log('double');
-			tempNumber += currentValue;
-			masterArray.push(parseInt(tempNumber));
-			console.log('tempNumber: ' + tempNumber);
-		}
-		else {
-			masterArray.push(parseInt(tempNumber));
-			console.log('Adding to master..');
-			var tempNumber = '';
-		}
-
-		
-		var previousPosition = currentPosition;
-	}
-	console.log(masterArray);
-
-};
-
-
-
-
-var foo = function(inputString) {
+var wordNumberCount = function(inputString) {
 
     var inputSplit = inputString.split('');
 	var numberFound = false;
@@ -80,7 +12,7 @@ var foo = function(inputString) {
 	
         var element = inputSplit[ i ];
 		 var  elementIndex = inputSplit.indexOf(element);
-        var elementIsNumber = foo2(element);
+        var elementIsNumber = findIfElementIsNumber(element);
 		
         if(elementIsNumber === true ) {
 			numberFound = true;
@@ -88,7 +20,7 @@ var foo = function(inputString) {
 			var  nextElement = inputSplit[ i + 1 ];
 
 				if( i + 1 < inputSplit.length) {
-					var nextElementIsNumber = foo2(nextElement);
+					var nextElementIsNumber = findIfElementIsNumber(nextElement);
 				}
 				else {
 					var nextElementIsNumber = false;
@@ -106,11 +38,11 @@ var foo = function(inputString) {
 	return totalCount;
 };
 
-var foo2 = function(element) {
+var findIfElementIsNumber = function(element) {
 	return isNaN(element) === false;
 };
 
-var foo9 = function() {
+var addNumbers = function() {
 
 	var totalCount = 0;
 
@@ -118,7 +50,7 @@ var foo9 = function() {
 
 		var currentWord = process.argv[  i  ];
 		console.log(currentWord);
-		var wordCount = foo(currentWord);
+		var wordCount = wordNumberCount(currentWord);
 		totalCount += wordCount;
 		
 	}
@@ -126,4 +58,16 @@ var foo9 = function() {
 	console.log("totalCount: " + totalCount);
 };
 
-foo9();
+//addNumbers();
+
+var longestWord = function() {
+	var wordArray = process.argv.slice(2);
+	var sortedWordArray = _.sortBy(wordArray, function(element) { return element.length } );
+	sortedWordArray.reverse();
+	var largestWord = sortedWordArray[ 0 ];
+	console.log('largest word: ' + largestWord);
+	return largestWord;
+};
+
+longestWord();
+
